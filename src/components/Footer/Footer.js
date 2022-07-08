8eed9ef11e62fc74a6eca0fd95a8bde151cb0b13
IshtiaqHasan <ishtiaqhasan1992@gmail.com>
2022-07-08 10:34:04 +0600

    footer included

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,10 +5,16 @@ import fb from '../../images/fb.png';
 import viber from '../../images/viber.png';
 import twiter from '../../images/twiter.png';
 import inst from '../../images/inst.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 const Footer = () => {
+    let getYear = () => {
+        let currentYear = new Date().getFullYear();
+        return currentYear;
+    };
     return (
-        <div>
+        <div className='allFooter'>
             <div className='soft-partner'>
                 <div className='soft-partner-heading'>
                     <h3>Looking for a <br />Software Partner?</h3>
@@ -18,22 +24,62 @@ const Footer = () => {
                 </div>
             </div>
             <div className='about-softcent'>
-                <div className='sotLogo'>
-                    <img src={softLogo} height='87px' width='132px' alt="" />
+                <div>
+                    <div className='sotLogo'>
+                        <img src={softLogo} height='87px' width='132px' alt="" />
+                    </div>
+                    <div className='softAddress'>
+                        <p>Get a fully stacked design team that will <br />give you accessible design, exhaustive <br />research, and an outstanding experience. </p>
+                        <h4>Email: info@softcent.eu</h4>
+                        <h4>Phone: +(372) 57836500</h4>
+                    </div>
+                    <div className='social-images'>
+                        <i className="fa fa-facebook-official" style={{ fontSize: '24px', padding: '5px' }}></i>
+                        <i className="fa fa fa-whatsapp" style={{ fontSize: '24px', padding: '5px' }}></i>
+                        <i className="fa fa-twitter" style={{ fontSize: '24px', padding: '5px' }}></i>
+                        <i className="fa fa-instagram" style={{ fontSize: '24px', padding: '5px' }}></i>
+                    </div>
                 </div>
-                <div className='softAddress'>
-                    <p>Get a fully stacked design team that will <br />give you accessible design, exhaustive <br />research, and an outstanding experience. </p>
-                    <h4>Email: info@softcent.eu</h4>
-                    <h4>Phone: +(372) 57836500</h4>
-                </div>
+                <div className='policy'>
+                    <div className='company'>
+                        <h5>Company</h5>
+                        <p>About Us</p>
+                        <p>Our Portfolio</p>
+                        <p>Our Product</p>
+                        <p>Our Team</p>
+                        <p>Our Pricing</p>
+                        <p>Testimonials</p>
+                        <p>Contact Us</p>
+                    </div>
+                    <div className='service'>
+                        <h5>Our Service</h5>
+                        <p>Marketing</p>
+                        <p>UI/UX Design</p>
+                        <p>Graphic Design (Branding)</p>
+                        <p>Development</p>
+                        <p>New Products</p>
+                    </div>
+                    <div className='supports'>
+                        <h5>Supports</h5>
+                        <p>FAQs</p>
+                        <p>Support Policy</p>
+                        <p>Private Policy</p>
+                        <p>Trams of Service</p>
+                        <p>Refund Policy</p>
+                    </div>
 
+                </div>
 
             </div>
-            <div className='social-images'>
-                <img src={fb} height='30px' width='40px' alt="" />
-                <img src={viber} height='26px' width='26px' alt="" />
-                <img src={twiter} height='26px' width='26px' alt="" />
-                <img src={inst} height='30px' width='30px' alt="" />
+            <div>
+                <hr className='line' />
+            </div>
+            <div className='copyright'>
+                <small >
+                    &copy;
+                    <span> {getYear()}, Softcent EU, A European IT Company </span>
+                    <br />
+                </small>
             </div>
         </div>
     );
